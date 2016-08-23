5fc11e2465ef87a12ff7c1524ed74dda690a4f8f
OlgaPushilina <olya.pushilina@gmail.com>
2016-08-23 14:36:45 -0700

    Созданы классы-помощники

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by t_aleksandr on 8/23/16.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+   public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+}
