fa91946e15708b652ed0c1831a71cd9ec0c3a42d
OlgaPushilina <olya.pushilina@gmail.com>
2016-09-09 09:51:03 -0700

    Контакты: лямбда-выражения (анонимные функции)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,18 +16,12 @@ public class ContactCreationTests extends TestBase {
       app.getNavigationHelper().gotoHomePage();
       List<ContactData> before = app.getContactHelper().getContactList();
       ContactData contact = new ContactData(
-              "Olga", "Test", "Tester", "QA", "100 Main Street San Francisco, CA", "516-29-08", "test1");
+              "Olga", "Test1", "Tester", "QA", "100 Main Street San Francisco, CA", "516-29-08", "test1");
       app.getContactHelper().createContact(contact);
       List<ContactData> after = app.getContactHelper().getContactList();
       Assert.assertEquals(after.size(), before.size() + 1);
 
-      int max = 0;
-      for (ContactData c : after) {
-        if (c.getId() > max) {
-          max = c.getId();
-        }
-      }
-      contact.setId(max);
+      contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
       before.add(contact);
       Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
