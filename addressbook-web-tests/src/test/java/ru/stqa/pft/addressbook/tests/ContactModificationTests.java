cd7822c48b8225042c83f939bd30152e65adb2de
OlgaPushilina <olya.pushilina@gmail.com>
2016-09-12 23:25:38 -0700

    Задание №11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,12 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 
 public class ContactModificationTests extends TestBase {
@@ -23,18 +25,15 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
-    Set<ContactData> before = app.contact().all();
-    ContactData modifiedContact = before.iterator().next();
-    ContactData contact = new ContactData().withId(modifiedContact.getId())
+    Contacts before = app.contact().all();
+    ContactData oldContact = before.iterator().next();
+    ContactData newContact = new ContactData().withId(oldContact.getId())
             .withFirstname("Olga").withLastname("Test1").withTitle("Tester").withCompany("QA")
             .withAddress("100 Main Street San Francisco, CA").withHomephone("516-29-08");
-    app.contact().modify(contact);
-    Set<ContactData> after = app.contact().all();
-    Assert.assertEquals(after.size(), before.size());
-
-    before.remove(modifiedContact);
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    app.contact().modify(newContact);
+    Contacts after = app.contact().all();
+    assertEquals(after.size(), before.size());
+    assertThat(after, equalTo(before.withModified(oldContact, newContact)));
   }
 
 }
